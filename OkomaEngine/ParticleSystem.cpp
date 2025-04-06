09940bf98c4099f31bda948a67455b7902a65ee7
SageEkpang <e018364m@student.staffs.ac.uk>
2025-04-06 18:12:27 +0100

    Further Particle System Implementation

--- OkomaEngine/ParticleSystem.cpp
+++ OkomaEngine/ParticleSystem.cpp
@@ -1,9 +1,56 @@
 #include "ParticleSystem.h"
 
-ParticleSystem::ParticleSystem(int maxParticleCount)
+ParticleSystem::ParticleSystem(OKTransform2<float> transform, float mass, unsigned int maxParticleCount, ParticleSpawnArea particleSpawnArea, ParticleAction particleAction)
 {
+	// NOTE: Set Transform
+	m_Transform = transform;
+
+	// NOTE: Fill Vector and reserve the set size for the particles
 	m_MaxParticleCount = maxParticleCount;
 	m_Particles.reserve(m_MaxParticleCount);
+
+	for (unsigned int i = 0; i < m_MaxParticleCount; ++i)
+	{
+		Particle particle = Particle(&m_Transform, mass);
+		m_Particles.push_back(particle);
+	}
+
+	// NOTE: Set and "Proccess" Spawn Area
+	m_ParticleSpawnArea = particleSpawnArea;
+	m_ParticleAction = particleAction;
+
+	m_ParticleAreaMap[PARTICLE_SPAWN_AREA_NONE] = ProcessSpawnAreaNone;
+	m_ParticleAreaMap[PARTICLE_SPAWN_AREA_CIRCLE] = ProcessSpawnAreaCircle;
+	m_ParticleAreaMap[PARTICLE_SPAWN_AREA_HALF_CIRCLE] = ProcessSpawnAreaHalfCircle;
+	m_ParticleAreaMap[PARTICLE_SPAWN_AREA_RECTANGLE] = ProcessSpawnAreaRectangle;
+	m_ParticleAreaMap[PARTICLE_SPAWN_AREA_TRIANGLE] = ProcessSpawnAreaTriangle;
+	m_ParticleAreaMap[PARTICLE_SPAWN_AREA_CAPSULE] = ProcessSpawnAreaCapsule;
+	m_ParticleAreaMap[PARTICLE_SPAWN_AREA_DONUT] = ProcessSpawnAreaDonut;
+	m_ParticleAreaMap[PARTICLE_SPAWN_AREA_EDGE] = ProcessSpawnAreaEdge;
+
+
+	// NOTE: Set and "Proccess" Action
+	m_ParticleActionMap[PARTICLE_ACTION_NONE] = ProcessActionNone;
+	m_ParticleActionMap[PARTICLE_ACTION_BURST_OUT] = ProcessActionBurstOut;
+	m_ParticleActionMap[PARTICLE_ACTION_BURST_IN] = ProcessActionBurstIn;
+
+	m_ParticleActionMap[PARTICLE_ACTION_SCREEN] = ProcessActionScreen;
+	m_ParticleActionMap[PARTICLE_ACTION_SCREEN_OUT] = ProcessActionScreenOut;
+	m_ParticleActionMap[PARTICLE_ACTION_SCREEN_IN] = ProcessActionScreenIn;
+
+	m_ParticleActionMap[PARTICLE_ACTION_FALL] = ProcessActionFall;
+	m_ParticleActionMap[PARTICLE_ACTION_RISE] = ProcessActionRise;
+	m_ParticleActionMap[PARTICLE_ACTION_RIGHT] = ProcessActionRight;
+	m_ParticleActionMap[PARTICLE_ACTION_LEFT] = ProcessActionLeft;
+
+	m_ParticleActionMap[PARTICLE_ACTION_SPRAY] = ProcessActionSpray;
+	m_ParticleActionMap[PARTICLE_ACTION_SPIRAL] = ProcessActionSpiral;
+
+	m_ParticleActionMap[PARTICLE_ACTION_FIRE] = ProcessActionFire;
+	m_ParticleActionMap[PARTICLE_ACTION_SMOKE] = ProcessActionSmoke;
+	m_ParticleActionMap[PARTICLE_ACTION_SPARK] = ProcessActionSpark;
+	m_ParticleActionMap[PARTICLE_ACTION_WAVE] = ProcessActionWave;
+
 }
 
 ParticleSystem::~ParticleSystem()
@@ -18,6 +65,9 @@ void ParticleSystem::Update(const float deltaTime)
 {
 	for (auto v : m_Particles)
 	{
+		// NOTE: Set the Spawn Point of the Particle (Spawn Area) and Process the Desired Action (Particle Action)
+		m_ParticleAreaMap[m_ParticleSpawnArea](v);
+		m_ParticleActionMap[m_ParticleAction](v);
 		v.Update(deltaTime);
 	}
 }
@@ -29,3 +79,101 @@ void ParticleSystem::Draw()
 		v.Draw();
 	}
 }
+
+void ParticleSystem::ProcessSpawnAreaNone(Particle& particle)
+{
+}
+
+void ParticleSystem::ProcessSpawnAreaCircle(Particle& particle)
+{
+}
+
+void ParticleSystem::ProcessSpawnAreaHalfCircle(Particle& particle)
+{
+}
+
+void ParticleSystem::ProcessSpawnAreaRectangle(Particle& particle)
+{
+}
+
+void ParticleSystem::ProcessSpawnAreaTriangle(Particle& particle)
+{
+}
+
+void ParticleSystem::ProcessSpawnAreaCapsule(Particle& particle)
+{
+}
+
+void ParticleSystem::ProcessSpawnAreaDonut(Particle& particle)
+{
+}
+
+void ParticleSystem::ProcessSpawnAreaEdge(Particle& particle)
+{
+}
+
+void ParticleSystem::ProcessActionNone(Particle& particle)
+{
+}
+
+void ParticleSystem::ProcessActionBurstOut(Particle& particle)
+{
+}
+
+void ParticleSystem::ProcessActionBurstIn(Particle& particle)
+{
+}
+
+void ParticleSystem::ProcessActionScreen(Particle& particle)
+{
+}
+
+void ParticleSystem::ProcessActionScreenOut(Particle& particle)
+{
+}
+
+void ParticleSystem::ProcessActionScreenIn(Particle& particle)
+{
+}
+
+void ParticleSystem::ProcessActionFall(Particle& particle)
+{
+}
+
+void ParticleSystem::ProcessActionRise(Particle& particle)
+{
+}
+
+void ParticleSystem::ProcessActionRight(Particle& particle)
+{
+}
+
+void ParticleSystem::ProcessActionLeft(Particle& particle)
+{
+}
+
+void ParticleSystem::ProcessActionSpray(Particle& particle)
+{
+
+}
+
+void ParticleSystem::ProcessActionSpiral(Particle& particle)
+{
+}
+
+void ParticleSystem::ProcessActionFire(Particle& particle)
+{
+}
+
+void ParticleSystem::ProcessActionSmoke(Particle& particle)
+{
+}
+
+void ParticleSystem::ProcessActionSpark(Particle& particle)
+{
+}
+
+void ParticleSystem::ProcessActionWave(Particle& particle)
+{
+}
+
--- OkomaEngine/ParticleSystem.h
+++ OkomaEngine/ParticleSystem.h
@@ -4,6 +4,8 @@
 #include "Particle.h"
 #include <memory>
 #include <vector>
+#include <utility>
+#include <map>
 // NOTE: In the collab project, rendering will have to be included here as well
 
 // NOTE: Variables to Manipulate for Each Area
@@ -20,7 +22,7 @@ enum ParticleSpawnArea
 {
 	PARTICLE_SPAWN_AREA_NONE,
 	PARTICLE_SPAWN_AREA_CIRCLE,
-	PARTICLE_SPAWN_HALF_CIRCLE,
+	PARTICLE_SPAWN_AREA_HALF_CIRCLE,
 	PARTICLE_SPAWN_AREA_RECTANGLE,
 	PARTICLE_SPAWN_AREA_TRIANGLE,
 	PARTICLE_SPAWN_AREA_CAPSULE,
@@ -30,6 +32,7 @@ enum ParticleSpawnArea
 
 enum ParticleAction
 {
+	PARTICLE_ACTION_NONE,
 	PARTICLE_ACTION_BURST_OUT,
 	PARTICLE_ACTION_BURST_IN,
 
@@ -55,35 +58,48 @@ class ParticleSystem
 {
 private:
 
+	// CUSTOM DATA TYPE
+	typedef void (*particle_func)(Particle&);
+	typedef std::map<ParticleSpawnArea, particle_func> particle_spawn_area_map;
+	typedef std::map<ParticleAction, particle_func> particle_action_map;
+
+	// TRANSFORM VARIABLE(s)
+	OKTransform2<float> m_Transform;
+	OKVector2<float> m_SpawnPosition;
+
 	// PARTICLE VARIABLE(s)
 	std::vector<Particle> m_Particles;
+
+	ParticleSpawnArea m_ParticleSpawnArea;
+	ParticleAction m_ParticleAction;
+
+	particle_spawn_area_map m_ParticleAreaMap;
+	particle_action_map m_ParticleActionMap;
+
 
 	// BASE VARIABLE(s)
 	unsigned int m_MaxParticleCount;
 	float m_Duration = { 1.0f };
 	bool m_IsLooping{ false };
 	float m_StartDelay{ 0.0f }; // NOTE: In Seconds
-	float m_StartLiftTime{ 5.0f };
+	float m_StartLifeTime{ 5.0f };
 	float m_StartSpeed{ 5.0f };
-	float m_StartSize{ 1.0f };
+	OKVector2<float> m_StartSize{ 1.0f, 1.0f };
+
 	OKVector2<float> m_Gravity{ 0.0f, 0.0f };
 	bool m_SimulateGravity{ false };
 	float m_SimulationSpeed{ 1.0f };
 
 
 	// EMISSION VARIABLE(s)
-	unsigned int m_RateOverTimer{ 5u };
-	unsigned int m_RateOverDistance{ 1u };
-
-
-	// SHAPE VARIABE(s)
-	ParticleSpawnArea m_ParticleSpawnArea;
+	unsigned int m_EmissionRateOverTime{ 5u };
+	unsigned int m_EmissionRateOverDistance{ 1u };
 
 	// NOTE: These will overwrite previous variables that were set
 
 	// VELOCITY OVER LIFE TIME VARIABLE(s)
-	OKVector2<float> m_StartingVelocityOverLiftTime;
-	OKVector2<float> m_EndingVelocityOverLiftTime;
+	OKVector2<float> m_StartingVelocityOverLifeTime;
+	OKVector2<float> m_EndingVelocityOverLifeTime;
 
 	// FORCE OVER LIFE TIME VARIABLE(s)
 	OKVector2<float> m_StartingForceOverLifeTime;
@@ -91,12 +107,12 @@ private:
 
 
 	// SIZE OVER LIFE TIME VARIABLE(s)
-	OKVector2<float> m_StartingSizeOverLiftTime;
-	OKVector2<float> m_EndingSizeOverLiftTime;
+	OKVector2<float> m_StartingSizeOverLifeTime;
+	OKVector2<float> m_EndingSizeOverLifeTime;
 
 	// SIZE BY SPEED VARIABLE(s) --> Links to Variables Above
-	float m_StartingSizeBySpeed;
-	float m_EndingSizeBySpeed;
+	OKVector2<float> m_StartingSizeBySpeed;
+	OKVector2<float> m_EndingSizeBySpeed;
 
 	// COLLISION
 	bool m_HasCollision;
@@ -104,7 +120,7 @@ private:
 public:
 
 	// CLASS FUNCTION(s)
-	ParticleSystem(int maxParticleCount);
+	ParticleSystem(OKTransform2<float> transform, float mass, unsigned int maxParticleCount, ParticleSpawnArea particleSpawnArea = PARTICLE_SPAWN_AREA_NONE, ParticleAction particleAction = PARTICLE_ACTION_NONE);
 	~ParticleSystem();
 
 
@@ -113,13 +129,106 @@ public:
 	void Draw();
 
 
+	// HELPER FUNCTION(s)
+
+		// NOTE: PARTICLE SPAWN AREA FUNCTION(s)
+		static void ProcessSpawnAreaNone(Particle& particle);
+		static void ProcessSpawnAreaCircle(Particle& particle);
+		static void ProcessSpawnAreaHalfCircle(Particle& particle);
+		static void ProcessSpawnAreaRectangle(Particle& particle);
+		static void ProcessSpawnAreaTriangle(Particle& particle);
+		static void ProcessSpawnAreaCapsule(Particle& particle);
+		static void ProcessSpawnAreaDonut(Particle& particle);
+		static void ProcessSpawnAreaEdge(Particle& particle);
+
+
+		// NOTE: PARTICLE ACTION FUNCTION(s)
+		static void ProcessActionNone(Particle& particle);
+
+		static void ProcessActionBurstOut(Particle& particle);
+		static void ProcessActionBurstIn(Particle& particle);
+
+		static void ProcessActionScreen(Particle& particle);
+		static void ProcessActionScreenOut(Particle& particle);
+		static void ProcessActionScreenIn(Particle& particle);
+
+		static void ProcessActionFall(Particle& particle);
+		static void ProcessActionRise(Particle& particle);
+		static void ProcessActionRight(Particle& particle);
+		static void ProcessActionLeft(Particle& particle);
+
+		static void ProcessActionSpray(Particle& particle);
+		static void ProcessActionSpiral(Particle& particle);
+
+		static void ProcessActionFire(Particle& particle);
+		static void ProcessActionSmoke(Particle& particle);
+		static void ProcessActionSpark(Particle& particle);
+		static void ProcessActionWave(Particle& particle);
+
+
 	// GETTER FUNCTION(s)
+
+		inline OKTransform2<float> GetTransform() const { return m_Transform; }
+		inline OKVector2<float> GetPosition() const { return m_Transform.position; }
+		inline OKVector2<float> GetScale() const { return m_Transform.scale; }
+		inline float GetRotation() const { return m_Transform.rotation; }
+
+		inline unsigned int GetMaxParticleCount() const { return m_MaxParticleCount; }
+		inline float GetDuration() const { return m_Duration; }
+		inline bool GetLooping() const { return m_IsLooping; }
+		inline float GetStartDelay() const { return m_StartDelay; }
+		inline float GetStartLifeTime() const { return m_StartLifeTime; }
+		inline float GetStartSpeed() const { return m_StartSpeed; }
+		inline OKVector2<float> GetStartSize() const { return m_StartSize; }
+
+		inline OKVector2<float> GetGravity() const { return m_Gravity; }
+		inline bool GetSimulateGravity() const { return m_SimulateGravity; }
+		inline float GetSimulationSpeed() const { return m_SimulationSpeed; }
+
+		inline unsigned int GetEmissionRateOverTime() const { return m_EmissionRateOverTime; }
+		inline unsigned int GetEmissionRateOverDistance() const { return m_EmissionRateOverDistance; }
+
+		inline OKVector2<float> GetStartingVelocityOverLifeTime() const { return m_StartingVelocityOverLifeTime; }
+		inline OKVector2<float> GetEndingVelocityOverLifeTime() const { return m_EndingVelocityOverLifeTime; }
+
+		inline OKVector2<float> GetStartingForceOverLifeTime() const { return m_StartingForceOverLifeTime; }
+		inline OKVector2<float> GetEndingForceOverLifeTime() const { return m_EndingForceOverLifeTime; }
+
+		inline OKVector2<float> GetStartingSizeBySpeed() const { return m_StartingSizeBySpeed; }
+		inline OKVector2<float> GetEndingSizeBySpeed() const { return m_EndingSizeBySpeed; }
 
 
 	// SETTER FUNCTION(s)
 
-	//ParticleSystem* GetInstance() { return this; }
+		inline void SetTransform(OKTransform2<float> transform) { m_Transform = transform; }
+		inline void SetPosition(OKVector2<float> position) { m_Transform.position = position; }
+		inline void SetScale(OKVector2<float> scale) { m_Transform.scale = scale; }
+		inline void SetRotation(float rotation) { m_Transform.rotation = rotation; }
 
+
+		inline void SetMaxParticleCount(unsigned int maxParticleCount) { m_MaxParticleCount = maxParticleCount; }
+		inline void SetDuration(float duration) { m_Duration = duration; }
+		inline void SetLooping(bool looping) { m_IsLooping = looping; }
+		inline void SetStartDelay(float startDelay) { m_StartDelay = startDelay; }
+		inline void SetStartLifeTime(float startLifeTime) { m_StartLifeTime = startLifeTime; }
+		inline void SetStartSpeed(float startSpeed) { m_StartSpeed = startSpeed; }
+		inline void SetStartSize(OKVector2<float> startSize) { m_StartSize = startSize; }
+
+		inline void SetGravity(OKVector2<float> gravity) { m_Gravity = gravity; }
+		inline void SetSimulateGravity(bool simulateGravity) { m_SimulateGravity = simulateGravity; }
+		inline void SetSimulationSpeed(float simulateSpeed) { m_SimulationSpeed = simulateSpeed; }
+
+		inline void SetEmissionRateOverTime(unsigned int emissionRateOverTime) { m_EmissionRateOverTime = emissionRateOverTime; }
+		inline void SetEmissionRateOverDistance(unsigned int emissionRateOverDistance) { m_EmissionRateOverDistance = emissionRateOverDistance; }
+
+		inline void SetStartingVelocityOverLifeTime(OKVector2<float> startingVelocityOverLifeTime) { m_StartingVelocityOverLifeTime = startingVelocityOverLifeTime; }
+		inline void SetEndingVelocityOverLifeTime(OKVector2<float> endingVelocityOverLifeTime) { m_EndingVelocityOverLifeTime = endingVelocityOverLifeTime; }
+
+		inline void SetStartingForceOverLifeTime(OKVector2<float> startingForceOverLifeTime) { m_StartingForceOverLifeTime = startingForceOverLifeTime; }
+		inline void SetEndingForceOverLifeTime(OKVector2<float> endingForceOverLifeTime) { m_EndingForceOverLifeTime = endingForceOverLifeTime; }
+
+		inline void SetStartingSizeBySpeed(OKVector2<float> startingSizeBySpeed) { m_StartingSizeBySpeed = startingSizeBySpeed; }
+		inline void SetEndingSizeBySpeed(OKVector2<float> endingSizeBySpeed) { m_EndingSizeBySpeed = endingSizeBySpeed; }
 };
 
 #endif
