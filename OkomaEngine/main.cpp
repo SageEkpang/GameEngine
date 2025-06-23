4071c844ce20e54ac355050b39be5aead0689a52
SageEkpang <e018364m@student.staffs.ac.uk>
2025-06-23 10:36:57 +0100

    fixing the delta time

--- OkomaEngine/main.cpp
+++ OkomaEngine/main.cpp
@@ -72,7 +72,7 @@ int main()
 	GameObjectEntity CircleObjectTwo;
 	CircleObjectTwo.m_Transform.position = OKVector2<float>(20, 200);
 	CircleObjectTwo.AddComponent<Rigidbody2DComponent>()->Construct(10, RIGIDBODY_MOVEMENT_TYPE_DYNAMIC);
-	CircleObjectTwo.AddComponent<CapsuleColliderComponent>()->Construct(40.0f, 80.0f);
+	CircleObjectTwo.AddComponent<RectangleColliderComponent>()->Construct(40.0f, 40.0f);
 	m_PhyMana.AddPhysicsObject(&CircleObjectTwo);
 
 
@@ -80,7 +80,7 @@ int main()
 	GameObjectEntity m_Floor;
 	m_Floor.m_Transform.position = OKVector2<float>(0.f, -200.f);
 	m_Floor.AddComponent<Rigidbody2DComponent>()->Construct(FLT_MAX, RIGIDBODY_MOVEMENT_TYPE_STATIC);
-	m_Floor.AddComponent<CapsuleColliderComponent>()->Construct(50.f, 100.f);
+	m_Floor.AddComponent<RectangleColliderComponent>()->Construct(500.f, 50.f);
 	m_PhyMana.AddPhysicsObject(&m_Floor);
 
 	// NOTE: Plinko
@@ -91,23 +91,26 @@ int main()
 
 	float t_StartTime = 0.0f;
 	t_StartTime = std::chrono::time_point_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now()).time_since_epoch().count() / 1000000.f;
-	OKTime::m_TimeScale = FPS_15;
+	OKTime::m_TimeScale = 1;
 
 	#pragma endregion
 
+	// SetTargetFPS(60);
 	while (!WindowShouldClose())
 	{
-		// UPDATE
-
+		// NOTE: Start of Delta Time
 		auto startTime = std::chrono::high_resolution_clock::now();
 
-
-		// DRAW
 		BeginDrawing();
 			ClearBackground(BLACK);
 
+		// UPDATE
+
 			camera.offset = Vector2{ GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f };
 			camera.zoom = 1.0f;
+
+
+		// DRAW
 
 			BeginMode2D(camera);
 
@@ -118,14 +121,7 @@ int main()
 
 			// DrawText(TextFormat("Position: %f", m_Object2.m_Transform.position.y), 0, 0, 40, GREEN);
 
-
-
-
 			rlPopMatrix();
-
-
-
-
 
 
 
@@ -133,25 +129,31 @@ int main()
 			rlScalef(1.0f, -1.0f, 1.0f);
 			// NOTE: Draw Here --------
 
-
-			m_PhyMana.Update(OKTime::m_DeltaTime);
+			// m_PhyMana.Update(OKTime::m_DeltaTime);
+			m_PhyMana.Update(FPS_60);
 			m_PhyMana.Draw();
-
-			auto endTime = std::chrono::high_resolution_clock::now();
-			auto start = std::chrono::time_point_cast<std::chrono::microseconds>(startTime).time_since_epoch().count();
-			auto end = std::chrono::time_point_cast<std::chrono::microseconds>(endTime).time_since_epoch().count();
-
-			OKTime::m_UnscaledDeltaTime = (float)(end - start) / 1000.f;
-			OKTime::m_DeltaTime = OKTime::m_UnscaledDeltaTime * OKTime::m_TimeScale;
-
-			OKTime::m_RealTimeSinceStartUp = end / 1000000.f - t_StartTime;
-			OKTime::m_Time = OKTime::m_RealTimeSinceStartUp * OKTime::m_TimeScale;
-			OKTime::m_FrameCount++;
 
 			rlPopMatrix();
 			EndMode2D();
 
 		EndDrawing();
+
+		// NOTE: End of Delta Time
+		auto endTime = std::chrono::high_resolution_clock::now();
+
+		// NOTE: Delta Time Assinging
+		auto start = std::chrono::time_point_cast<std::chrono::microseconds>(startTime).time_since_epoch().count();
+		auto end = std::chrono::time_point_cast<std::chrono::microseconds>(endTime).time_since_epoch().count();
+
+		OKTime::m_UnscaledDeltaTime = (float)(end - start) / 1000000.f;
+		OKTime::m_DeltaTime = OKTime::m_UnscaledDeltaTime * OKTime::m_TimeScale;
+
+		OKTime::m_RealTimeSinceStartUp = end / 1000000.f - t_StartTime;
+		OKTime::m_Time = OKTime::m_RealTimeSinceStartUp * OKTime::m_TimeScale;
+		OKTime::m_FrameCount++;
+
+		printf("DeltaTime: %f\n", OKTime::GetDeltaTime());
+		printf("FrameTime: %f\n\n", GetFrameTime());
 	}
 
 	CloseAudioDevice();
